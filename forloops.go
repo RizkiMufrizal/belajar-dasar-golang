89a4d36a9ea1851d60382d578e743b5b86765869
RizkiMufrizal <mufrizalrizki@gmail.com>
2022-11-13 15:19:29 +0700

    learn for

--- forloops.go
+++ forloops.go
@@ -9,4 +9,35 @@ func main() {
 		fmt.Println("perulangan ke", counter)
 		counter++
 	}
+
+	for i := 1; i <= 10; i++ {
+		fmt.Println("perulangan i ke", i)
+	}
+
+	contohSlice := []map[string]string{
+		{"name": "rizki"},
+		{"name": "mufrizal"},
+	}
+
+	for i := 0; i < len(contohSlice); i++ {
+		fmt.Println(contohSlice[i])
+		fmt.Println(contohSlice[i]["name"])
+	}
+
+	for i, m := range contohSlice {
+		fmt.Println("index", i, "=", m["name"])
+	}
+
+	for _, m := range contohSlice {
+		fmt.Println(m["name"])
+	}
+
+	contohMap := map[string]string{
+		"first": "rizki",
+		"last":  "mufrizal",
+	}
+
+	for _, s2 := range contohMap {
+		fmt.Println(s2)
+	}
 }
