d111139200385755498e51ccb5354b7d531a529a
RizkiMufrizal <mufrizalrizki@gmail.com>
2022-12-13 22:41:31 +0700

    lean thread

---
+++ belajar-golang-goroutine/threads_test.go
@@ -0,0 +1,56 @@
+package belajargolanggoroutine
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestThread(t *testing.T) {
+	group := sync.WaitGroup{}
+
+	for i := 1; i <= 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCpu := runtime.NumCPU()
+	fmt.Println("CPU", totalCpu)
+
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Thread", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Goroutine", totalGoroutine)
+
+	group.Wait()
+}
+
+func TestChangeThread(t *testing.T) {
+	group := sync.WaitGroup{}
+
+	for i := 1; i <= 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCpu := runtime.NumCPU()
+	fmt.Println("CPU", totalCpu)
+
+	runtime.GOMAXPROCS(20)
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Thread", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Goroutine", totalGoroutine)
+
+	group.Wait()
+}
