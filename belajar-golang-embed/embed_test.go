8b81483f531ea1ca84500b81e7475038d85f8205
RizkiMufrizal <mufrizalrizki@gmail.com>
2022-12-17 17:19:00 +0700

    learn embed file to byte array (slice)

--- belajar-golang-embed/embed_test.go
+++ belajar-golang-embed/embed_test.go
@@ -3,6 +3,8 @@ package belajargolangembed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -15,4 +17,14 @@ var version2 string
 func TestFileToString(t *testing.T) {
 	fmt.Println(version)
 	fmt.Println(version2)
+}
+
+//go:embed picture1.jpg
+var picture []byte
+
+func TestFileToByteArray(t *testing.T) {
+	err := ioutil.WriteFile("picture2.jpg", picture, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
