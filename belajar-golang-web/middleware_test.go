fb3d098b493a4525e283b7f5daec5011f62838f3
RizkiMufrizal <mufrizalrizki@gmail.com>
2022-12-18 23:00:39 +0700

    learn middleware

---
+++ belajar-golang-web/middleware_test.go
@@ -0,0 +1,59 @@
+package belajargolangweb
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+type LogMiddleware struct {
+	Handler http.Handler
+}
+
+func (middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Before Execute Handle")
+	middleware.Handler.ServeHTTP(writer, request)
+	fmt.Println("After Execute Handle")
+}
+
+type ErrorHandler struct {
+	Handler http.Handler
+}
+
+func (errorHandler *ErrorHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	defer func() {
+		err := recover()
+		fmt.Println("RECOVER", err)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(writer, "error : %s", err)
+		}
+	}()
+	errorHandler.Handler.ServeHTTP(writer, request)
+}
+
+func TestMiddleware(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Handler Executed")
+		fmt.Fprint(writer, "Hello Middleware")
+	})
+	mux.HandleFunc("/panic", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Handler Panic Executed")
+		panic("Ups")
+	})
+
+	logMiddleware := &LogMiddleware{
+		Handler: mux,
+	}
+
+	errorHandler := &ErrorHandler{
+		Handler: logMiddleware,
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: errorHandler,
+	}
+	server.ListenAndServe()
+}
