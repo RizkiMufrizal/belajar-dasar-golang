6ecdf00af9d3f0c29f94cc9a6b970558ccfb0f1b
RizkiMufrizal <mufrizalrizki@gmail.com>
2022-12-18 17:52:56 +0700

    learn serve file

--- belajar-golang-web/file_server_test.go
+++ belajar-golang-web/file_server_test.go
@@ -2,6 +2,7 @@ package belajargolangweb
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"net/http"
 	"testing"
@@ -20,6 +21,28 @@ func TestFileServer(t *testing.T) {
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+//go:embed resources/ok.html
+var ok string
+
+//go:embed resources/notfound.html
+var notfound string
+
+func ServeFile(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		fmt.Fprint(writer, ok)
+	} else {
+		fmt.Fprint(writer, notfound)
+	}
+}
+
+func TestServeFile(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
 	}
 	server.ListenAndServe()
 }
