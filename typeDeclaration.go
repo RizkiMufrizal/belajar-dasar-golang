d943e391fdf25e09ab2a2f6f7364160380e778f0
RizkiMufrizal <mufrizalrizki@gmail.com>
2022-12-28 09:53:10 +0700

    learn json

--- typeDeclaration.go
+++ typeDeclaration.go
@@ -7,4 +7,14 @@ func main() {
 
 	var noKtp NoKTP = "1234567890"
 	fmt.Println(noKtp)
+
+	name := "rizki"
+	nama := &name
+
+	fmt.Println(name)
+	fmt.Println(nama)
+}
+
+func sample(name *string)  {
+
 }
